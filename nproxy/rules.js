d77b960801840ffc8af84e6211562ad1bae418d5
みん <min.pso2@gmail.com>
2015-01-19 02:26:47 +0900

    fix loading font using nproxy

--- nproxy/rules.js
+++ nproxy/rules.js
@@ -1,6 +1,6 @@
 var path = require('path');
 
 module.exports = [{
-	pattern: /^https?:\/\/rawgit\.com\/blue-monday\/pso2(?:-atwiki)?\/master\/dist\//,
+	pattern: /^https?:\/\/rawgit\.com\/blue-monday\/pso2(?:-atwiki)?\/master\/dist\/(?=scripts|styles)/,
 	responder: path.join(__dirname, '../dist')
 }];
