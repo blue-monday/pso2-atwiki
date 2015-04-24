dfdd642e78745cb1a1da3712856fb5b231bf2273
みん <min.pso2@gmail.com>
2015-04-24 10:53:56 +0900

    refactor

--- src/scripts/youtubepopup.js
+++ src/scripts/youtubepopup.js
@@ -36,39 +36,41 @@ module.exports = function() {
     });
   });
 
-  if (typeof window.onYouTubePlayerReady === 'function')
-    var _onYouTubePlayerReady = window.onYouTubePlayerReady;
+  if (typeof onYouTubePlayerReady === 'function') {
+    window.onYouTubePlayerReady = (function() {
+      var original = window.onYouTubePlayerReady;
 
-  window.onYouTubePlayerReady = function(playerid) {
-    $('#' + playerid).trigger('youtube:playerready', [playerid]);
+      return function(playerid) {
+        $('#' + playerid).trigger('youtube:playerready', [playerid]);
 
-    if (_onYouTubePlayerReady)
-      _onYouTubePlayerReady.apply(this, arguments);
-  };
+        return original.apply(this, arguments);
+      };
+    })();
+  }
 
   $(document)
-  .on('click', '.youtube-popup-link', onPopupLinkClick)
-  .on('youtube:playerready', '.youtube-popup', toggleTimerecorder)
-  .on('dblclick', '.youtube-popup-titlebar', function() {
-    $(this).closest('.youtube-popup').toggleClass('collapse-video');
-  })
-  .on('click', '.youtube-popup-titlebar-buttons .btn-timerecorder', toggleTimerecorder)
-  .on('click', '.youtube-popup-titlebar-buttons .btn-openprev', openNextPrev)
-  .on('click', '.youtube-popup-titlebar-buttons .btn-opennext', openNextPrev)
-  .on('click', '.youtube-popup-titlebar-buttons .btn-closepopup', closePopup)
-  .on('click', '.youtube-popup-timerecorder td.link', onRecordButtonClick)
-  .on('focus click', '.youtube-popup-timerecorder .format', function() {
-    $(this).select();
-  });
+    .on('click', '.youtube-popup-link', onPopupLinkClick)
+    .on('youtube:playerready', '.youtube-popup', toggleTimerecorder)
+    .on('dblclick', '.youtube-popup-titlebar', function() {
+      $(this).closest('.youtube-popup').toggleClass('collapse-video');
+    })
+    .on('click', '.youtube-popup-titlebar-buttons .btn-timerecorder', toggleTimerecorder)
+    .on('click', '.youtube-popup-titlebar-buttons .btn-openprev', openNextPrev)
+    .on('click', '.youtube-popup-titlebar-buttons .btn-opennext', openNextPrev)
+    .on('click', '.youtube-popup-titlebar-buttons .btn-closepopup', closePopup)
+    .on('click', '.youtube-popup-timerecorder td.link', onRecordButtonClick)
+    .on('focus click', '.youtube-popup-timerecorder .format', function() {
+      $(this).select();
+    });
 
   function onPopupLinkClick(event) {
-    // jshint validthis:true
+    /* jshint validthis:true */
     event.preventDefault();
 
     var $this = $(this).blur();
     var playerid = $this.attr('data-playerid');
 
-    /* global alert */
+    /* global alert:true */
     if ($('#' + playerid).length) {
       alert('同じ動画は2つ以上同時に開けません');
       return;
@@ -141,11 +143,11 @@ module.exports = function() {
       .addClass('youtube-popup-timerecorder-footer');
 
     var table = $('<table/>');
-    var thead = $('<thead><tr><th></th><th>wave1</th><th>wave2</th><th>wave3</th><th>wave4</th><th>wave5</th><th>wave6</th></tr></thead>');
+    var thead = $('<thead><tr><th><th>wave1<th>wave2<th>wave3<th>wave4<th>wave5<th>wave6</thead>');
     var tbody = $('<tbody/>');
-    var tr1 = $('<tr><th>開始時刻</th></tr>');
-    var tr2 = $('<tr><th>終了時刻</th></tr>');
-    var tr3 = $('<tr><th>所要時間</th></tr>');
+    var tr1 = $('<tr><th>開始時刻</tr>');
+    var tr2 = $('<tr><th>終了時刻</tr>');
+    var tr3 = $('<tr><th>所要時間</tr>');
 
     table.append(thead, tbody);
     tbody.append(tr1, tr2, tr3);
@@ -231,6 +233,7 @@ module.exports = function() {
     var popup = $this.closest('.youtube-popup');
     var object = popup.find('object');
     var t = Math.round(object.get(0).getCurrentTime());
+
     $this.text(secondsToTime(t)).attr('data-seconds', t);
 
     var colIndex = $this.attr('data-col');
@@ -252,11 +255,13 @@ module.exports = function() {
 
     var totalSeconds = 0;
     var timerecorder = $this.closest('.youtube-popup-timerecorder');
+
     timerecorder.find('.lap').each(function(i, cell) {
       cell = $(cell);
       text += cell.text() + '|';
       totalSeconds += cell.attr('data-seconds') | 0;
     });
+
     text += secondsToTime(totalSeconds) + '|';
     timerecorder.find('.format').val(text);
   }
@@ -264,6 +269,7 @@ module.exports = function() {
   function toggleTimerecorder() {
     // jshint validthis:true
     var popup = $(this);
+
     if (!popup.is('.youtube-popup'))
       popup = popup.closest('.youtube-popup');
 
@@ -276,12 +282,17 @@ module.exports = function() {
     var $this = $(this);
     var diff = $this.hasClass('btn-opennext') ? 1 : -1;
     var popup = $this.closest('.youtube-popup');
-    var nextPlayerid = popup.find('object').attr('id').replace(/\d+$/, function(d) { return +d + diff; });
+    var nextPlayerid = popup.find('object').attr('id').replace(/\d+$/, function(d) {
+      return +d + diff;
+    });
+
     var link = $('a[data-playerid="' + nextPlayerid + '"]');
+
     if (!link.length) {
       alert('動画がありません');
       return;
     }
+
     link.click();
     $('#' + nextPlayerid).closest('.youtube-popup').offset(popup.offset());
     popup.remove();
