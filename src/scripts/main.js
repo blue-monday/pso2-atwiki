7306513702fbf9ee90a8128e269196d8853c6bd9
みん <min.pso2@gmail.com>
2015-04-30 13:27:51 +0900

    refactor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,14 @@
 'use strict';
 
 (function() {
-  var toastr = require('toastr');
   var plugins = require('./plugins');
   var pages = require('./pages');
 
+  var toastr = require('toastr');
   toastr.options.timeOut = 2000;
 
   require('./default');
 
-  plugins.loadPlugins();
   pages.loadPages();
+  plugins.loadPlugins();
 })();
